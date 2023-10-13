44612e16ac142b7124886ec5d865cad364165e06
Augusto Gabriel Rodrigues dos Santos <augusto.santos@dataeasy.com.br>
2023-10-13 17:48:53 -0300

    validação do usuario

--- src/main/java/br/com/gutobiel/todolist/filter/FilterTaskAuth.java
+++ src/main/java/br/com/gutobiel/todolist/filter/FilterTaskAuth.java
@@ -3,9 +3,12 @@ package br.com.gutobiel.todolist.filter;
 import java.io.IOException;
 import java.util.Base64;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
+import at.favre.lib.crypto.bcrypt.BCrypt;
+import br.com.gutobiel.todolist.user.IUserRepository;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
@@ -13,6 +16,10 @@ import jakarta.servlet.http.HttpServletResponse;
 
 @Component
 public class FilterTaskAuth extends OncePerRequestFilter {
+
+    @Autowired
+    private IUserRepository userRepository;
+
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
@@ -37,11 +44,24 @@ public class FilterTaskAuth extends OncePerRequestFilter {
 
                 
                 //Validar usuario
+                var user = this.userRepository.findByUsername(username);
+                if  (user == null){
+                    response.sendError(401);
+                    } else {
+                    //Validar senha
+                    var passwordVerify = BCrypt.verifyer().verify(password.toCharArray(), user.getPassword());
+                    if (passwordVerify.verified){
+                        filterChain.doFilter(request, response);
+                    } else {
+                        response.sendError(401);
+                    }
+                
 
-                //Validar senha
+
 
                 //Segue viagem
 
-                filterChain.doFilter(request, response);
+                
+            }
     }
 }
